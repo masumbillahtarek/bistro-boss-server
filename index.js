16b1da261e3c21dfa741022c0ea65f9d8b7a4c4f
Masum Billah Tarek <mdmasumbillah245524@gmail.com>
2025-08-31 01:12:04 +0600

    final

--- index.js
+++ index.js
@@ -221,6 +221,76 @@ app.get('/payments/:email',verifyToken,async(req,res)=>{
   const result=await paymentsCollection.find(query).toArray()
   res.send(result)
 })
+app.get('/admin-stats',verifyToken,verifyAdmin,async(req,res)=>{
+  const users=await userCollection.estimatedDocumentCount()
+  const menuItems=await menuCollection.estimatedDocumentCount()
+  const orders=await paymentsCollection.estimatedDocumentCount()
+  //This is not the best way
+// const payments=await paymentsCollection.find().toArray()
+  //const revenue=payments.reduce((total,payment)=>total+payment.price,0)
+  //Now The Best Way
+const result=await paymentsCollection.aggregate([
+  {
+    $group:{
+      _id:null,
+      totalRevenue:{
+        $sum:'$price'
+      }
+    }
+  }
+]).toArray()
+const revenue=result.length>0?result[0].totalRevenue:0;
+
+  res.send({
+    users,menuItems,orders,revenue
+  })
+})
+
+app.get('/order-stats',async(req,res)=>{
+  const result=await paymentsCollection.aggregate([
+    {
+      $unwind:'$menuItemIds'
+    }   ,
+    {
+  $addFields: {
+    menuItemIds: { $toObjectId: "$menuItemIds" }
+  }
+  },
+     {
+      $lookup:{
+        from:'menu',
+        localField:'menuItemIds',
+        foreignField:'_id',
+        as:'menuItems'
+      }
+    } ,
+      {
+      $unwind:'$menuItems'
+    },
+    {
+      $group:{
+        _id:'$menuItems.category',
+        quantity:{$sum: 1},
+        revenue:{$sum:'$menuItems.price'}
+      }
+    },
+    {
+      $project:{
+        _id:0,
+        category:'$_id',
+        quantity:'$quantity',
+        revenue:'$revenue'
+      }
+    }
+    /*
+ 
+
+
+    */
+   
+  ]).toArray();
+  res.send(result)
+})
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
